f7a68c09cdbbbdfb668c11ee0ae69476a2a4ece6
bellps <malvezzi.isabella@gmail.com>
2025-08-04 19:33:43 -0300

    feat: creates folder scructure and updates requirements.txt

---
+++ app/classes/prompt_formatter.py
@@ -0,0 +1,34 @@
+from app.classes.prompt_request import PromptRequest
+
+
+class PromptFormatter():
+    def __init__(self, prompt_request: PromptRequest) -> None:
+        self.prompt_request = prompt_request
+
+    def format(self) -> str:
+        return f"""
+        {self.instructions()}
+        Using the SQL schema:
+        {self.prompt_request.sql_schema}
+        Generate a SQL query for the following question:
+        {self.prompt_request.prompt}
+        """
+
+    def instructions(self) -> str:
+        return f"""
+        # System Instructions
+
+        ## Basic Rules
+        You are a relational database specialist, with the focus of creating SQL queries based on a given database schema and a question from the user.
+
+        Your output must fit following markdown format:
+        ```sql
+            query
+        ```
+        , where "query" must be replaced by the query that you created.
+
+        The queries must follow the PostgreSQL dialect.
+        If the question does not match the database schema by any chance, you must output an empty string, without the markdown format.
+
+        <end_of_turn><start_of_turn>user
+        """
---
+++ app/main.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI, HTTPException
+import uvicorn
+from app.classes.prompt_request import PromptRequest
+from huggingface_hub import login
+import logging
+import os
+from dotenv import load_dotenv
+from app.classes.slonito_model import SlonitoModel
+
+load_dotenv()
+
+login(token = os.getenv("HF_TOKEN"))
+
+logger = logging.getLogger("uvicorn.error")
+logger.setLevel(logging.DEBUG)
+
+app = FastAPI()
+slonito = SlonitoModel()
+
+
+@app.post("/generate")
+async def generate_text(request: PromptRequest):
+    try:
+        logger.debug("Starting generation...")
+
+        response: str = slonito.generate_response(request)
+
+        logger.debug("Generation completed!")
+
+        return {"response": response}
+    except Exception as e:
+        logger.debug(e)
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/marco")
+async def marco():
+    try:
+        return {"response": "polo"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get("/")
+async def root():
+    return {"message": "SLONITO: Up and Running"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=4000, log_level="debug", host="0.0.0.0")
+# uvicorn main:app --reload --host 0.0.0.0 --port 4000 --log-level debug
